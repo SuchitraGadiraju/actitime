47bba83b6150228195540295ccb5bdd2e5397001
Suchitra <Suchitra@TY-Suchitra>
2022-10-06 15:43:14 +0530

    changes the values of URL,browser

---
+++ src/test/java/script/ValidLogin.java
@@ -0,0 +1,34 @@
+package script;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import generic.BaseTest;
+import page.EnterTimeTrackPage;
+import page.Loginpage;
+
+public class ValidLogin extends BaseTest{
+
+	@Test(priority = 1)
+	public void testValidLogin() {
+//		1. Enter valid user name
+		Loginpage loginPage=new Loginpage(driver);
+		loginPage.setUserName("admin");
+		test.info("Enter valid user name");
+		
+//		2. Enter Valid password
+		loginPage.setPassword("manager");
+		test.info("Enter Valid password");
+		
+//		3. click on login button
+		loginPage.clickLoginButton();
+		test.info("click on login button");
+		
+//		4. Verify that home page should be displayed
+		EnterTimeTrackPage ettPage=new EnterTimeTrackPage(driver);
+		boolean result = ettPage.verifyLogoutLinkIsDisplayed(wait);
+		Assert.assertEquals(result, true,"Home page is not displayed");
+		test.pass("Home page is displayed");
+	}
+}
